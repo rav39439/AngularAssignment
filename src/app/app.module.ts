9cee67968e166c778b6c8ad5c27038efc6ca80c2
rav3434 <ravish_cg@yahoo.com>
2022-10-22 15:27:45 +0530

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,13 +37,13 @@ import {MatIconModule} from '@angular/material/icon';
     AppRoutingModule,
     MatTableModule,
     AngularFireModule.initializeApp({
-      apiKey: "AIzaSyAZ9x_QbhCfY9aEsqxEiYhWtfIRNGl_qeo",
-      authDomain: "mynewproject-ae49a.firebaseapp.com",
-      projectId: "mynewproject-ae49a",
-      storageBucket: "mynewproject-ae49a.appspot.com",
-      messagingSenderId: "939005557600",
-      appId: "1:939005557600:web:ee766070db4bd2dadfbc05",
-      measurementId: "G-YFKTT151SN"
+      apiKey: "AIzaSyCLOm6HzkKiJ9GZn1daP2k5C8BeDSeK31I",
+  authDomain: "employeedetailsapp.firebaseapp.com",
+  projectId: "employeedetailsapp",
+  storageBucket: "employeedetailsapp.appspot.com",
+  messagingSenderId: "3665313687",
+  appId: "1:3665313687:web:7e1b7137ffb3c9e4118ed2",
+  measurementId: "G-C7YMVRVKM6"
 
      
     }),
